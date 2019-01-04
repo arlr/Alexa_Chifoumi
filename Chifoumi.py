12be869811f182f742f4ff994bcee4b9a61d200a
arlr <arlrcode@gmail.com>
2019-01-04 13:13:27 +0100

    corection

--- Chifoumi.py
+++ Chifoumi.py
@@ -1,7 +1,7 @@
 #!/sys/bin/env python3
 
 from flask import Flask, render_template
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question
 
 app = Flask(__name__)
 ask = Ask(app, '/Chifoumi')
