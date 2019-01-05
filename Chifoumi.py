b71dfc6b7f4ddc9c488cfdf54c6571e00c101173
arlr <arlrcode@gmail.com>
2019-01-05 13:40:02 +0100

    Ajout de mode de jeu

--- Chifoumi.py
+++ Chifoumi.py
@@ -2,19 +2,61 @@
 
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question
+from random import *
 
 app = Flask(__name__)
 ask = Ask(app, '/Chifoumi')
 
+item_list = ["pierre", "feuille", "ciseaux" ]
+
+def regle(Alexa , Player):
+    #Alexa Win
+    print("Fonction regle")
+    if (((str(Alexa) == "pierre") and (str(Player) == "ciseau")) or ((str(Alexa) == "feuille") and (str(Player) == "pierre")) or ((str(Alexa) == "ciseaux") and (str(Player) == "feuille"))):
+        text = "Alexa à gagnée"
+        return(text)
+    else:
+        text = "Vous avez gagné"
+        return(text)
+
 @ask.launch
 def start_skill():
-    welcome_message = "Bonjour dite moi quand vous êtes prêts"  #Message que vas dire Alexa
+    welcome_message = "Bonjour, voulez-vous que je sois le matre du jeu ou que je joue avec vous ?"  #Message que vas dire Alexa
     return question(welcome_message)
 
-@ask.intent('StartIntent')
-def start():
-    text = "D'accord tenez vous prêts .......   3 ...., 2 ..., 1....  Chiii Fou Mi !!!!"
-    return statement(text)
+#@ask.intent('ChoixIntent')
+#def Choix():
+#    phrase = "D'accord. Dite moi quand vous êtes prêts ?"
+#    return question(phrase)
+
+@ask.intent('MDJ_StartIntent')
+def MDJ_start():
+    phrase = "D'accord c'est parti  .......   3 ...., 2 ..., 1....  Chiii Fou My !!!!"
+    return statement(phrase)
+
+@ask.intent('JeuxIntent')
+def Jeux_Start():
+    phrase = "D'accord c'est parti, dite moi ce que vous avez choisi ?"
+    return question(phrase)
+
+@ask.intent('ItemIntentt')
+def In_game(Item):
+    print("Fonction In_game")
+    AlexaItem = item_list[randint(0,2)]
+    alexaText ="Alexa : " + AlexaItem
+    print(alexaText)
+    joueurText = "Joueur : " + Item
+    print(joueurText)
+
+    if str(AlexaItem) == str(Item):
+        phrase = "J'ai choisi " + Item +" aussi nous avons donc fais égalité"
+        return(phrase)
+    else:
+        phrase = regle(AlexaItem, Item)
+        return(phrase) 
+
+
+
 
 @ask.session_ended
 def session_ended():
---
+++ test.py
@@ -0,0 +1,35 @@
+from random import *
+
+item_list = ["pierre", "feuille", "ciseaux" ]
+
+def regle(Alexa , Player):
+    #Alexa Win
+    print("Fonction regle")
+    if (((str(Alexa) == "pierre") and (str(Player) == "ciseau")) or ((str(Alexa) == "feuille") and (str(Player) == "pierre")) or ((str(Alexa) == "ciseaux") and (str(Player) == "feuille"))):
+        text = "Alexa à gagnée"
+        return(text)
+    else:
+        text = "Vous avez gagné"
+        return(text)
+
+def In_game(Item):
+    print("Fonction In_game")
+    AlexaItem = item_list[randint(0,2)]
+    alexaText ="Alexa : " + AlexaItem
+    print(alexaText)
+    joueurText = "Joueur : " + Item
+    print(joueurText)
+
+    if str(AlexaItem) == str(Item):
+        phrase = "J'ai choisi " + Item +" aussi nous avons donc fais égalité"
+        return(phrase)
+    else:
+        phrase = regle(AlexaItem, Item)
+        return(phrase) 
+
+
+if __name__ == '__main__':
+    
+    itemJoueur = input("Entree pierre, ciseaux ou feuille : ")
+    text  = In_game(itemJoueur)
+    print(text)
