c6d025785addcb5aa7ecf4370e1393c5495990fb
arlr <arlrcode@gmail.com>
2019-01-08 12:47:47 +0100

    hgjfkdls

--- Chifoumi.py
+++ Chifoumi.py
@@ -71,4 +71,4 @@ def session_ended():
     return "{}", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True,ssl_context=('cert.pem', 'key.pem'))
+    app.run( port=80, debug=True,ssl_context=('cert.pem', 'key.pem'))
